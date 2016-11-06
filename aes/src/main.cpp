17db335e3745df3bbad2cf63442b3022cc0bd477
Fan Jiang <fanjiang@thoughtworks.com>
2016-11-06 15:11:32 -0500

    main now call the aes

--- aes/src/main.cpp
+++ aes/src/main.cpp
@@ -1,38 +1,26 @@
 #ifndef UNIT_TEST // disable program main loop while unit testing in progress
 
 #include <Arduino.h>
-#include "calculator.h"
 #include "aes.h"
 
-Calculator calc;
-
-void blink_once(unsigned int delay_ms)
-{
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(delay_ms);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(delay_ms);
-}
-
-
 void setup() {
+    delay(3000);
     Serial.begin(9600);
     Serial.println("Program started!");
 }
 
 void loop() {
-    Serial.print("Addition: ");
-    Serial.println(calc.add(25, 17));
-    blink_once(200);
-    Serial.print("Subtraction: ");
-    Serial.println(calc.sub(10, 3));
-    blink_once(200);
-    Serial.print("Multiplication: ");
-    Serial.println(calc.mul(3, 3));
-    blink_once(200);
-    Serial.print("Division: ");
-    Serial.println(calc.div(100, 3));
-    blink_once(200);
-}
+    uint8_t key[] = { 0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c };
+    uint8_t iv[]  = { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f };
+    uint8_t in[64]  = "helloworld";
+    uint8_t out[64];
+    uint8_t buffer[64];
+    Serial.println("Encrypt...");
+    aes::AES128_CBC_encrypt_buffer(buffer, in, 64, key, iv);
 
+    Serial.println("Decrypt...");
+    aes::AES128_CBC_decrypt_buffer(out, buffer, 64, key, iv);
+    Serial.println((char *)out);
+    delay(5000);
+}
 #endif
